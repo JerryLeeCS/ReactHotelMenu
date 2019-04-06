4087ab01d8a532e52b7be96eddd57a508a3c4ca6
Jerry <jerry@Jerrys-MBP.socal.rr.com>
2019-04-06 15:47:17 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -6,20 +6,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="paper-container">
+          <div className="grid-layout">
+            <div className="top-navigation-bar">
+              SEE ALL LAS VEGAS HOTELS
+            </div>
+            <div className="left-col">
+              <div>img container might replace with just image later</div>
+              <div>list</div>
+            </div>
+            <div className="right-col">
+              <div>hotel profile</div>
+              <div>hotel information component</div>
+          </div>
+          </div>
+        </div>
       </div>
     );
   }
