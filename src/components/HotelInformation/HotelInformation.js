6ddcd56b0f929879d406ea132ff876c266e11b2c
Jerry <jerry@Jerrys-MBP.socal.rr.com>
2019-04-08 19:23:50 -0700

    Finished professional information's tab

--- src/components/HotelInformation/HotelInformation.js
+++ src/components/HotelInformation/HotelInformation.js
@@ -1,9 +1,57 @@
-import React from '../../../node_modules/react'
+import React, { Component } from 'react'
+import './HotelInformation.css'
+export default class HotelInformation extends Component {
+  constructor(){
+    super()
+    this.setActiveTab = this.setActiveTab.bind(this)
+    this.state = {
+      activeTab: 0
+    }
+  }
 
-export default function HotelInformation(props) {
-  return (
-    <div>
-      Hotel Information
-    </div>
-  )
+  setActiveTab(tab) {
+    this.setState({
+      activeTab: tab
+    })
+  }
+
+  render() {
+
+    let tabContent
+
+    switch (this.state.activeTab) {
+      case 1:
+        tabContent = <div>Details</div>
+        break
+      case 2:
+        tabContent = <div>Location</div>
+        break
+      default:
+        tabContent = <div>Description</div>
+        break
+    }
+
+
+    return (
+      <div id="hotel-information">
+        <div className="tabs">
+          <div onClick={() => this.setActiveTab(0)} className={this.state.activeTab === 0 ? "tab-highlighted tab flex-1" : "tab flex-1"}>
+            <span className="tab-name">DESCRIPTION</span>            
+          </div>
+
+          <div onClick={() => this.setActiveTab(1)} className={this.state.activeTab === 1 ? "tab-highlighted tab flex-1" : "tab flex-1"}>
+            <span className="tab-name">DETAILS</span>
+          </div>
+
+          <div onClick={() => this.setActiveTab(2)} className={this.state.activeTab === 2 ? "tab-highlighted tab flex-1" : "tab flex-1"}>
+            <span className="tab-name">LOCATION</span>
+          </div>
+        </div>
+        
+        <div className="tab-content">
+          {tabContent}
+        </div>
+      </div>
+    )
+  }
 }
